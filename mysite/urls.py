fc5bd0d4244da3fd6888f34aaf8cbc8bd389ffab
Tetey <aglagunda@up.edu.ph>
2015-05-16 17:15:35 +0800

    FINISHED!

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
